08ca7dcaa852aa3663b7210410fa3500afeee84c
mntone <sd8@live.jp>
2015-09-30 09:32:03 +0900

    Separates entities and logic.

---
+++ Mntone.SplatoonClient/Mntone.SplatoonClient.Entities/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Mntone.SplatoonClient.Entities")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("mntone")]
+[assembly: AssemblyProduct("Mntone.SplatoonClient.Entities")]
+[assembly: AssemblyCopyright("Copyright (C) 2015- mntone")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("ja")]
+
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
+
+[assembly: InternalsVisibleTo("Mntone.SplatoonClient")]
