901659ec12f3e592ba22996058aedba7cbdd812f
mntone <sd8@live.jp>
2015-10-06 11:10:59 +0900

    Change source code as Nintendo updates SplatNet (bug fix).

--- Mntone.SplatoonClient/Mntone.SplatoonClient/SplatoonContextFactory.cs
+++ Mntone.SplatoonClient/Mntone.SplatoonClient/SplatoonContextFactory.cs
@@ -11,10 +11,8 @@ namespace Mntone.SplatoonClient
 			var authorizer = new NintendoNetworkAuthorizer();
 			var requestToken = await authorizer.GetRequestTokenAsync(SplatoonConstantValues.AUTH_FORWARD_URI).ConfigureAwait(false);
 			var accessToken = await authorizer.Authorize(requestToken, new AuthenticationToken(username, password), SplatoonHelper.GetSessionValue).ConfigureAwait(false);
-			var requestToken2 = await authorizer.GetRequestTokenAsync(SplatoonConstantValues.AUTH_FORWARD_URI).ConfigureAwait(false);
-			var accessToken2 = await authorizer.Authorize(requestToken2, new AuthenticationToken(username, password), SplatoonHelper.GetSessionValue).ConfigureAwait(false);
 			authorizer.Dispose();
-			return new SplatoonContext(accessToken2);
+			return new SplatoonContext(accessToken);
 		}
 	}
 }
